4c5e0dc1c7c097a8b61acd9dd508cc45ad66a330
1xtend <kokitoki0504@gmail.com>
2023-01-08 13:04:06 +0100

    first commit

--- color-switch.js
+++ color-switch.js
@@ -8,11 +8,11 @@ const body = document.querySelector('body');
 
 let interval;
 
+// Functions
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-// Functions
 
 function startChangingColor() {
   interval = setInterval(() => {
